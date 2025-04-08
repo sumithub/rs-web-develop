c9825c71391d1d1490c7edbd414047b2b42c1158
Parminder Kaur <parminder@fkointech.com>
2025-04-08 17:15:42 +0530

    Sign-up Form

--- src/app/components/form/Checkbox2.js
+++ src/app/components/form/Checkbox2.js
@@ -1,14 +1,14 @@
-"use client"
-export default function Checkbox2({ label, name, checked, onChange }) {
-    return <div>
-        <div className="h-5">
-            {/* <div>
-                <label className="text-base text-text-primary">{label}</label>
-                <div className="text-base text-text-primary font-bold">{name}</div>
-            </div> */}
-            <input type="checkbox" className="h-5 w-5 ml-auto"
-                onChange={() => { onChange(!checked) }}
-                checked={checked} />
-        </div>
-    </div>
+"use client";
+
+export default function Checkbox2({ id = "", }) {
+    return (
+        <>
+            <input
+                type="checkbox"
+                required={true}
+                id={id}
+                className="accent-primary w-4 h-4 rounded-sm border border-[#ccc]"
+            />
+        </>
+    );
 }
