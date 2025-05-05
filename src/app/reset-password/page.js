4a4f106ed76aa407a33275734a65a69f224f0882
Bikramjit <bikramjit@fkointech.com>
2025-05-05 15:29:28 +0530

    create components and pages

---
+++ src/app/reset-password/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ResetPassword() {
+  return (
+    <div>Reset Password</div>
+  )
+}
+
+export default ResetPassword
---
+++ src/components/VerificationEmail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function VerificationEmail() {
+  return (
+    <div>Your Email Has Been Verified Successful!</div>
+  )
+}
+
+export default VerificationEmail
