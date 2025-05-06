486a48081f981d7320b721df5aaf7f540f7b5b18
Kulwinder Singh <kulwinder@fkointech.com>
2025-05-06 10:56:35 +0530

    .

--- src/app/forgot-password/page.js
+++ src/app/forgot-password/page.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import AuthLayout from "../../components/common/AuthLayout";
+import Input from '../../components/form/Input';
+import Link from 'next/link';
+import Image from 'next/image';
 
 function ForgotPassword() {
     return (<AuthLayout>
-                <div>Forgot Password</div>
-            </AuthLayout>
+        <div>
+            <h2 className="text-[34px] leading-none font-semibold text-secondary capitalize text-center">Forgot password</h2>
+            <p className="text-xs pt-2.5 pb-[25px] capitalize text-center text-[#616E7C]">A password reset link has been sent to your email</p>
+            <div>
+                <Input
+                    label="Email ID"
+                    name="name"
+                    inputType="text"
+                    placeholder="Enter Email ID "
+                    isRequired={true}
+                    icon="/images/close.svg"
+                    error="Incorrect Email"
+                />
+            </div>
+            <button className="text-white text-lg font-medium bg-primary hover:bg-white hover:text-primary w-full mt-5 py-3 rounded-[10px] border border-primary ">Send Reset Link</button>
+            <div className='flex justify-center mt-5'>
+                <Link href="/login" className="flex gap-[15px]">
+                    <Image src="/images/arrow.svg" alt='arrow.svg' width={20} height={20} />
+                    <h2 className='text-sm text-secondary'>Back To Login</h2>
+                </Link>
+            </div>
+        </div>
+    </AuthLayout>
     )
 }
 
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -8,10 +8,10 @@ import Link from 'next/link';
 import Image from 'next/image';
 function LoginPage() {
   return (<>
-    <AuthLayout>
+    <AuthLayout >
       <div>
         <h2 className="text-[34px] leading-none font-semibold text-secondary capitalize text-center">Login to your account</h2>
-        <p className="text-xs pt-2.5 pb-[25px] capitalize text-center text-[#616E7C]">Hey! We soar you working welcome back!</p>
+        <p className="text-xs pt-2.5 capitalize text-center text-[#616E7C]">Hey! We soar you working welcome back!</p>
         <div>
           <Input
             label="Email ID"
@@ -36,7 +36,7 @@ function LoginPage() {
               <Checkbox2 class_="border-text-3" />
               <h2 className='text-sm capitalize text-secondary'>Remember me</h2>
             </div>
-            <Link href="/">
+            <Link href="/forgot-password">
               <h2 className='text-sm capitalize text-primary'>Forgot Password ?</h2>
             </Link>
           </div>
@@ -45,7 +45,7 @@ function LoginPage() {
               <Image src="/images/error.svg" alt='error.svg' width={15} height={14} />
               <h2 className="text-xs text-danger capitalize">Invalid email or password. Please try again.</h2>
             </div>
-            <button className="text-white text-lg font-medium bg-primary hover:bg-white hover:text-primary w-full mt-2.5 py-3 rounded-[10px] border border-primary ">Login</button>
+            <button className="text-white text-lg font-medium bg-primary hover:bg-white hover:text-primary w-full mt-2.5 py-3 rounded-[10px] border border-primary cursor-pointer">Login</button>
             <h2 className='text-sm capitalize text-secondary pt-2.5 text-center'>Don&#39;t have an account? <Link href="/register" className='text-primary underline underline-offset-3'>Sign Up</Link></h2>
           </div>
         </div>
