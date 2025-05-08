f1cc3ad55ef64c3a674e92b209d25e87e1eb67dd
rajni62 <rajni@fkointech.com>
2025-05-08 18:02:28 +0530

    Added filters & inputs

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,19 @@ import Link from "next/link";
 export default function Home() {
 
   return (<>
-    <div className="text-secondary text-5xl font-semibold text-center capitalize my-80">
+    <div className="text-secondary text-3xl font-semibold text-center capitalize my-20">
       <div className="flex flex-col gap-y-5">
-        <Link href="/dashboard">Dashboard</Link>
-        <Link href="/campaign-dashboard">Campaign Dashboard</Link>
-        <Link href="/review">review</Link>
         <Link href="/login">Login</Link>
         <Link href="/reset-password">Reset password</Link>
         <Link href="/change-email">Change Email</Link>
         <Link href="/verification-email">verification email </Link>
         <Link href="/verify-email">verify email </Link>
+        <Link href="/dashboard">Dashboard</Link>
+        <Link href="/campaign-dashboard">Campaign Dashboard</Link>
+        <Link href="/review">review</Link>
+        <Link href="/users">Users Management</Link>
+        <Link href="/manage-campaigns">Manage Campaigns</Link>
+        <Link href="/campaigns-templates">Campaigns templates</Link>
       </div>
     </div>
   </>)
