912e80e1d4522ca0a45e4ac3ce8b685da840d5ec
Bikramjit <bikramjit@fkointech.com>
2025-05-12 13:52:51 +0530

    template model

--- src/components/Models/AddTemplate.js
+++ src/components/Models/AddTemplate.js
@@ -7,19 +7,19 @@ import Select from '../form/Select'
 function AddTemplate({ onClose, onSave }) {
   return <Model onClose={onClose} title="Invite New User" modalClass="w-[60%]!">
     <div>
-      <Select label="Template Type">
+      <Select label="Template Type" isRequired={true} selectClass_="py-3.5! px-2.5! focus:border-primary/60!">
         <option value="email template">Email Template</option>
       </Select>
-      <Input label="Template Name" isRequired={true} placeholder="Enter Name"/>
-      <Input label="Subject Line" isRequired={true} placeholder="Enter Line"/>
-      <Input label="Sender Name" isRequired={true} placeholder="Enter Sender Name"/>
-      <Input label="Sender Email" isRequired={true} placeholder="Enter Sender Email"/>
+      <Input label="Template Name" isRequired={true} placeholder="Enter Name" />
+      <Input label="Subject Line" isRequired={true} placeholder="Enter Line" />
+      <Input label="Sender Name" isRequired={true} placeholder="Enter Sender Name" />
+      <Input label="Sender Email" isRequired={true} placeholder="Enter Sender Email" />
     </div>
 
-    <div>
-      <CancelButton/>
-      <SecondaryButton title=""
-      <SecondaryButton title="Save & Activate"/>
+    <div className="grid grid-cols-2 gap-3 mt-5">
+      <CancelButton />
+      <SecondaryButton title="Save & Draft" />
+      <SecondaryButton title="Save & Activate" />
     </div>
   </Model>
 }
