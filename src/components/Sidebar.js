31cf3c237d300c34295347e5d898bfca4c3a0d8c
rajni62 <rajni@fkointech.com>
2025-05-12 13:25:35 +0530

    Sidebar links

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/router"
 import { usePathname } from "next/navigation";
+import { title } from "process";
 
 export default function Sidebar() {
     const pathname = useRouter.pathname
@@ -11,7 +12,11 @@ export default function Sidebar() {
 
     const list = [
         { title: "Dashboard", link: "/dashboard" },
-        { title: "Customers", link: "/Campaigns", submenu: [{ title: "Campaign Dashboard", link: "/campaign-dashboard" }] },
+        { title: "Customers", link: "/campaigns", submenu: [{ title: "User Management", link: "/users" }] },
+
+        { title: "Reviews", link: "/reviews", submenu: [{ title: "Review", link: "/review" }] },
+
+        { title: "Campaigns", link: "/campaigns", submenu: [{ title: "Campaign Dashboard", link: "/campaign-dashboard" }, { title: "Manage Campaigns", link: "/manage-campaigns" }, { title: "Campaigns Templates", link: "/campaigns-templates" }] },
     ]
 
     return <div className="relative">
@@ -23,25 +28,30 @@ export default function Sidebar() {
                 </div>
                 <div className="relative h-full flex flex-col justify-between overflow-y-auto custom-scrollbar pb-10 pt-10">
                     <div>
-                        {/* <button className="cursor-pointer">
-                            <Image src="/images/location.svg" alt="location" height={48} width={48} unoptimized={true} />
-                        </button> */}
+                        <div className="px-3 mb-4">
+                            <div className="flex gap-1 items-center px-4 py-3 text-sm rounded-[10px] bg-primary text-white">
+                                <Image src="/sidebarIcons/location.svg" alt="location" height={20} width={20} unoptimized={true} />
 
-                        <ul className="flex flex-col gap-y-5 px-3">
+                                <div className="text-xs font-medium line-clamp-1">4517 Washington Ave. Manchester, Kentucky 39495</div>
+                                <button className="cursor-pointer"><Image src="/images/arrow-up.svg" alt="arrow" height={20} width={20} /></button>
+
+                                <button className="cursor-pointer"><Image src="/images/add1.svg" alt="add" height={25} width={25} /></button>
+                            </div>
+                        </div>
+
+                        <ul className="flex flex-col gap-y-3 px-3">
                             {list.map((e, i) => {
                                 const submenu = e?.submenu
                                 return <MenuItem key={i} e={e} i={i} submenu={submenu} />
                             })}
                         </ul>
-
-
                         <div>
                         </div>
                     </div>
 
                     <div>
                         <div className="border-b-2 border-border2 mb-8" />
-                        <Image src="/images/avatar1.png" alt="avatar" height={56} width={56} className="h-14 w-14 rounded-full border border-border2" />
+                        <Image src="/images/avatar1.png" alt="avatar" height={56} width={56} className="h-14 w-14 rounded-full border border-border2 mx-auto" />
                     </div>
                 </div>
             </div>
@@ -62,21 +72,19 @@ const MenuItem = ({ i, e, submenu }) => {
     return <li key={i}>
         {submenu ? <div>
             <div onClick={() => { setOpenSubMenu(!openSubMenu) }}
-                className={`${openSubMenu ? "from-transparent to-border-color bg-bc2" : "hover:from-transparent hover:to-border-color"} transition-all duration-300 ease-in relative px-4 py-3 laptop:text-sm tablet:text-xs flex items-center justify-between cursor-pointer`}>
-                <div className="flex items-center">
-                    <span className="flex items-center justify-center">
-                        {/* <Icon icon={e.i} className="laptop:text-lg tablet:text-sm" /> */}
-                    </span>
-                    <span className="capitalize leading-[normal]">{e.title}</span>
+                className={`${openSubMenu ? "bg-light" : ""} relative px-4 py-3 text-sm flex items-center justify-between cursor-pointer mb-1`}>
+                <div className="flex items-center gap-2">
+                    <Image src="/sidebarIcons/customer.svg" alt="customer" height={20} width={20} />
+                    <span className="capitalize leading-[normal] text-sm text-text3 font-medium">{e.title}</span>
                 </div>
                 <Image src="/images/arrow-down.svg" alt="arrow" height={20} width={20} unoptimized={true} className={`transition duration-400 ease-in-out ${openSubMenu ? "rotate-180" : "rotate-0"}`} />
             </div>
             {openSubMenu && <ul>
-                {submenu && submenu.map((sub, i) => <li key={i} >
+                {submenu && submenu.map((sub, i) => <li key={i}>
                     <Link href={sub.link}
-                        className={`${(pathname === sub.link || pathname === (sub.link + "/[id]")) ? "bg-border-color border-l-3 border-primary" : "hover:to-bc2"} transition-all duration-300 ease-in before:transition-all flex items-center pr-4 pl-10 py-2.5 laptop:text-sm tablet:text-xs`}>
+                        className={`${(pathname === sub.link || pathname === (sub.link + "/[id]")) ? "bg-dark text-primary" : "text-text3"} relative flex items-center px-4 py-3 text-sm rounded-[10px]`}>
                         <span className="flex items-center justify-center">
-                            <Image src="/sidebarIcons/dashboard.svg" alt="dashboard" height={22} width={22} unoptimized={true} />
+                            <Image src="/sidebarIcons/message.svg" alt="message" height={22} width={22} unoptimized={true} />
                         </span>
                         <span className="ml-2 capitalize leading-[normal]">{sub.title}</span>
                     </Link>
@@ -84,7 +92,7 @@ const MenuItem = ({ i, e, submenu }) => {
                 )}
             </ul>}
         </div> :
-            <Link href={e.link} className={`${(pathname === e.link || pathname === (e.link + "/[id]")) ? "bg-dark text-primary" : "hover:to-bc2"} transition-all duration-300 ease-in relative flex items-center px-4 py-3 laptop:text-sm tablet:text-xs rounded-[10px]`}>
+            <Link href={e.link} className={`${(pathname === e.link || pathname === (e.link + "/[id]")) ? "bg-dark text-primary" : "text-text3"} relative flex items-center px-4 py-3 text-sm rounded-[10px]`}>
                 <span className="flex items-center justify-center">
                     <Image src="/sidebarIcons/dashboard.svg" alt="dashboard" height={22} width={22} unoptimized={true} />
                 </span>
