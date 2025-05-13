28f136d1cad42017335c9536c46c6654d8ab06d2
rajni62 <rajni@fkointech.com>
2025-05-13 16:46:35 +0530

    Created Campagin detail page

---
+++ src/app/manage-campaigns/detail/page.js
@@ -0,0 +1,103 @@
+import AdminLayout from "../../../components/AdminLayout"
+import ProgressBar from "../../../components/ProgressBar"
+import CancelButton from "../../../components/common/CancelButton"
+import SecondaryButton from "../../../components/common/SecondaryButton"
+import Input from "../../../components/form/Input"
+import CampaignCard from "../../../components/CampaignCard"
+import Select from "../../../components/form/Select"
+import Radio from "../../../components/form/Radio"
+import Image from "next/image"
+
+export default function Detail() {
+    return <AdminLayout>
+        <ProgressBar />
+        <div className="bg-white p-5">
+            <div className="flex items-center justify-between mb-4">
+                <div className="text-secondary text-xl font-medium">Create New Campaign</div>
+                <div className="flex items-center gap-2">
+                    <CancelButton title="Collapse All" class_="text-sm! font-normal!" />
+                    <SecondaryButton title="Expand All" class_="text-sm! font-normal!" />
+                </div>
+            </div>
+
+            <div>
+                <CampaignCard title="Campaign Details" status="Active">
+                    <hr className="border-b border-border-color my-2" />
+                    <div className="grid grid-cols-2 gap-3">
+                        <Input label="Campaign Name" placeholder="Enter Name" isRequired={true} inputClass="bg-white!" />
+                        <div>
+                            <div className="text-sm text-secondary font-medium capitalize mt-3 mb-2">Description</div>
+                            <textarea
+                                placeholder="Write a content post"
+                                rows={1} className="rounded text-sm bg-white  w-full focus:border-primary focus:outline-0 focus-visible:outline-0 py-3.5 px-2.5 text-sm text-secondary! rounded-lg" />
+                        </div>
+                    </div>
+                </CampaignCard>
+            </div>
+
+            <div>
+                <CampaignCard title="Targeting" status="Active">
+                    <hr className="border-b border-border-color my-2" />
+                    <div className="flex items-center justify-between">
+                        <div className="text-secondary text-sm font-medium capitalize">Select Customers from List</div>
+                        <SecondaryButton title="Add Customers" class_="text-sm! font-normal!" />
+                    </div>
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-center gap-2 my-5">
+                                <Image src="/images/warning.svg" alt="warning" height={22} width={22} />
+                                <div className="text-danger text-lg font-semibold capitalize">5 customers are already in an active campaign.?</div>
+                            </div>
+                            <Select defaultOption="Exclude Duplicates" selectClass_="bg-white!"></Select>
+                        </div>
+                        <Select label="Cooldown Period" isRequired={true} defaultOption="7 Days" selectClass_="bg-white! py-3! focus:border-primary/60!"></Select>
+
+                        <div className="border border-primary bg-[#0396FF1a] rounded-[10px] py-1.5 px-3 capitalize w-full text-base text-primary font-medium flex items-center justify-between mt-4">
+                            <div>Total Selected Customers</div>
+                            <div className="flex items-center gap-2">
+                                <div>250 Customers</div>
+                                <Image src="/images/eye1.svg" alt='eye' height={16} width={16} unoptimized={true} />
+                            </div>
+                        </div>
+                    </div>
+                </CampaignCard>
+            </div>
+
+            <div>
+                <CampaignCard title="Template Selection" status="Active">
+                    <hr className="border-b border-border-color my-2" />
+                    <div className="flex gap-3 my-4">
+                        <div className="text-sm text-secondary">Campaign Type<span className="text-danger">*</span></div>
+                        <div className="flex">
+                            <Radio label="Email" inputClass="mb-0!" labelClass="font-normal!" />
+                            <Radio label="SMS" inputClass="mb-0!" labelClass="font-normal!" />
+                            <Radio label="Both" inputClass="mb-0!" labelClass="font-normal!" />
+                        </div>
+                    </div>
+
+                    <div className="flex items-center justify-between">
+                        <div className="text-sm text-secondary">Primary Email Template<span className="text-danger">*</span></div>
+
+                        <SecondaryButton title="Template selection" class_="text-sm! font-normal!" />
+                    </div>
+
+                    <div className="bg-white p-3 rounded-lg">
+                        <div className="bg-dark rounded-lg p-2">
+                            <div className="flex items-center justify-between">
+                                <div>
+                                    <div className="text-secondary text-sm font-medium">Nature Template</div>
+                                </div>
+
+                                <div className="flex items-center gap-3">
+                                    <button className="bg-[#0396FF1a] p-2 rounded-lg flex gap-2 items-center justify-center text-xs text-primary font-medium"> <Image src="/images/eye1.svg" alt='eye' height={16} width={16} unoptimized={true} />Preview</button>
+
+                                    <button className="bg-[#0396FF1a] p-2 rounded-lg flex gap-2 items-center justify-center text-xs text-primary font-medium w-[85px]"> <Image src="/images/edit2.svg" alt='eye' height={14} width={14} unoptimized={true} />Edit</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </CampaignCard>
+            </div>
+        </div>
+    </AdminLayout>
+}
--- src/components/form/Radio.js
+++ src/components/form/Radio.js
@@ -1,3 +1,4 @@
+"use client"
 export default function Radio({ disabled = false, label, checked, labelClass = "", inputClass = "", class_ = "", name, onChange, value }) {
     return (<div className={`text-sm ${class_}`}>
         <label className="flex items-center gap-2 cursor-pointer">
@@ -9,7 +10,7 @@ export default function Radio({ disabled = false, label, checked, labelClass = "
                     }
                 }}
                 type="radio" name={name} checked={checked} value={value} className={`${inputClass} border-4 border-border-color h-5 w-5 rounded-full text-text mb-4`} />
-            <div className={`${labelClass} xl:text-[15px] font-medium text-sm capitalize xl:ml-1 ml-[2px] whitespace-pre`}>{label} </div>
+            <div className={`${labelClass} font-medium text-sm capitalize xl:ml-1 ml-[2px] whitespace-pre`}>{label} </div>
         </label>
     </div>
     )
