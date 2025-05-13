1c43cb9ca7530a632156ed17ebffcea8e5b8cd42
Bikramjit <bikramjit@fkointech.com>
2025-05-13 11:37:12 +0530

    model

---
+++ src/components/Models/manage-campaigns/ScheduleCampaign.js
@@ -0,0 +1,14 @@
+import DatePicker from '@/components/form/DatePicker'
+import React from 'react'
+
+function ScheduleCampaign() {
+  return (
+    <div>
+        <div>
+            <DatePicker />
+        </div>
+    </div>
+  )
+}
+
+export default ScheduleCampaign
