1ebaba8502a27d7380fdc5513e5f928f70b5b72b
Bikramjit <bikramjit@fkointech.com>
2025-05-13 13:59:29 +0530

    model

---
+++ src/components/Models/manage-campaigns/SelectedFromCustomers.js
@@ -0,0 +1,115 @@
+import SecondaryButton from "../../../components/common/SecondaryButton"
+import Search from "../../form/Search"
+import Model from "../Model"
+import TableOrder from '../../../components/TableOrder'
+import PaginationDemo from '../../../components/Pagination'
+import Checkbox from '../../../components/form/Checkbox'
+
+
+
+function SelectedCustomers({ onClose, onSave }) {
+    return (
+        <Model onClose={onClose} title="schedule campaign" modalClass="w-[60%]!" modalBodyClass="max-h-[90vh]!">
+            <div>
+                <div className="flex items-center justify-between mb-3">
+                    <Search placeholder="Search by Filter by name, email, phone" />
+                    <SecondaryButton title="Add Selected"></SecondaryButton>
+                </div>
+            </div>
+
+            <div className="table-class">
+                <table className="w-full">
+                    <thead>
+                        <tr>
+                            <th><TableOrder title="Customer Name" /></th>
+                            <th><TableOrder title="Email" /></th>
+                            <th><TableOrder title="Phone" /></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div className="flex items-start gap-2">
+                                    <Checkbox />
+                                    <div>John Doe</div>
+                                </div>
+                            </td>
+                            <td>john@example.com</td>
+                            <td>+91 9876543210</td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <div className="flex items-start gap-2">
+                                    <Checkbox />
+                                    <div>John Doe</div>
+                                </div>
+                            </td>
+                            <td>john@example.com</td>
+                            <td>+91 9876543210</td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <div className="flex items-start gap-2">
+                                    <Checkbox />
+                                    <div>John Doe</div>
+                                </div>
+                            </td>
+                            <td>john@example.com</td>
+                            <td>+91 9876543210</td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <div className="flex items-start gap-2">
+                                    <Checkbox />
+                                    <div>John Doe</div>
+                                </div>
+                            </td>
+                            <td>john@example.com</td>
+                            <td>+91 9876543210</td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <div className="flex items-start gap-2">
+                                    <Checkbox />
+                                    <div>John Doe</div>
+                                </div>
+                            </td>
+                            <td>john@example.com</td>
+                            <td>+91 9876543210</td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <div className="flex items-start gap-2">
+                                    <Checkbox />
+                                    <div>John Doe</div>
+                                </div>
+                            </td>
+                            <td>john@example.com</td>
+                            <td>+91 9876543210</td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <div className="flex items-start gap-2">
+                                    <Checkbox />
+                                    <div>John Doe</div>
+                                </div>
+                            </td>
+                            <td>john@example.com</td>
+                            <td>+91 9876543210</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <PaginationDemo />
+            </div>
+        </Model>
+    )
+}
+
+export default SelectedCustomers
