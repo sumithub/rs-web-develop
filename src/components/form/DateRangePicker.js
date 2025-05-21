8a6a38512d3ee2adfd55ff9f9568e4bce8c49e09
rajni62 <rajni@fkointech.com>
2025-05-21 13:28:51 +0530

    Added save button on DatePicker

--- src/components/form/DateRangePicker.js
+++ src/components/form/DateRangePicker.js
@@ -7,7 +7,7 @@ import 'react-date-range/dist/theme/default.css';
 
 function DateRange() {
     const [open, setOpen] = useState(false)
-     const ref = useRef(null);
+    const ref = useRef(null);
     useEffect(() => {
         document.addEventListener('keydown', (event) => {
             if (event.key === 'Escape') {
@@ -44,21 +44,25 @@ function DateRange() {
             onClick={toggleDatePicker}
             className="cursor-pointer flex items-center gap-2 border border-border-color rounded-lg py-[7.7px]! px-2 capitalize text-[13px] text-text3 w-full focus-visible:outline-none shrink-0"        >
             Date Range Selector
-            <Image src="/images/calendar1.svg" alt='calendar' height={16} width={16} unoptimized={true} className='' />
+            <Image src="/images/calendar1.svg" alt='calendar' height={16} width={16} unoptimized={true} />
         </button>
 
         {open &&
-            <div className='overflow-hidden absolute top-full right-0 border border-dark rounded-lg shadow-[0px_0px_40px_rgba(0,_0,_0,_0.08)]'>
-                <DateRangePicker
-                    showDateDisplay={false}
-                    onChange={item => setState([item.selection])}
-                    showSelectionPreview={true}
-                    moveRangeOnFirstSelection={false}
-                    months={2}
-                    ranges={state}
-                    direction="horizontal"
-                />
-            </div>}
+            <div>
+                <div className='overflow-hidden absolute top-full right-0 border border-dark rounded-lg shadow-[0px_0px_40px_rgba(0,_0,_0,_0.08)]'>
+                    <DateRangePicker
+                        showDateDisplay={false}
+                        onChange={item => setState([item.selection])}
+                        showSelectionPreview={true}
+                        moveRangeOnFirstSelection={false}
+                        months={2}
+                        ranges={state}
+                        direction="horizontal"
+                    />
+                    <button onClick={() => { setOpen(false) }} className="text-white text-lg font-medium bg-primary hover:bg-white hover:text-primary w-1/5 py-1 rounded-[10px] border border-primary cursor-pointer absolute left-0! bottom-3 ml-5">Save</button>
+                </div>
+            </div>
+        }
     </div>)
 }
 
