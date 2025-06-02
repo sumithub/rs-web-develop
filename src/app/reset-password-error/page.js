afaf5042dc585b61b04fa55ee91e519d203f78bc
Parminder Kaur <parminder@fkointech.com>
2025-06-02 18:09:01 +0530

    build fixes

--- src/app/reset-password-error/page.js
+++ src/app/reset-password-error/page.js
@@ -3,8 +3,15 @@
 import Error from "../../components/common/Error"
 import AuthLayout from "../../components/common/AuthLayout"
 import { useSearchParams } from "next/navigation"
+import { Suspense } from "react"
 
-export default function ResetPasswordError() {
+export default function ResetPasswordError_() {
+    return (<Suspense>
+        <ResetPasswordError />
+    </Suspense>)
+}
+
+const ResetPasswordError = () => {
     const searchParams = useSearchParams()
     const error = searchParams.get('error')
 
