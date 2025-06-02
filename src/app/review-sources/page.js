f02be4d0c72845c040277a08388dd36221a8e7d2
Kulwinder Singh <kulwinder@fkointech.com>
2025-06-02 18:36:17 +0530

    Popular Review Sources

--- src/app/review-sources/page.js
+++ src/app/review-sources/page.js
@@ -40,35 +40,42 @@ export default function ReviewSources() {
             <div className="pt-[15px]">
                 <h2 className="text-lg font-semibold">Popular Review Sources</h2>
                 <div className="pt-[15px] grid grid-cols-3 gap-5">
-                    <div className="p-[15px] shadow-sm rounded-[15px]">
-                        <div className="flex items-start justify-between">
-                            <Image unoptimized={true} src="/images/google2.svg" alt="google2" width={87} height={36} />
-                            <Status status="Connected" />
-                        </div>
-                        <div>
-                            <h2 className="text-base font-medium pt-2.5">Google Reviews</h2>
-                            <hr className="border border-secondary/5 my-[15px]" />
-                            <Input
-                                label="URL"
-                                placeholder=""
-                                hideOptional={true}
-                                isRequired={true}
-                                icon="/images/add-link.svg"
-                            />
-                        </div>
-                        <div>
-                            <button className="text-lg font-medium bg-danger py-3 rounded-[10px] text-white">
-                                Disconnect
-                            </button>
-                            <button>
-                                <Image unoptimized={true} src="/images/edit.svg" alt="edit" width={46} height={46} />
-                            </button>
-                        </div>
-                    </div>
+                    <ReviewCard />
                 </div>
             </div>
+        </div >
+    </AdminLayout >
+    )
+}
+function ReviewCard() {
+    return (
+        <div className="p-[15px] shadow-sm rounded-[15px]">
+            <div className="flex items-start justify-between">
+                <Image unoptimized={true} src="/images/google2.svg" alt="google2" width={87} height={36} />
+                <Status status="Connected" />
+            </div>
+            <div>
+                <h2 className="text-base font-medium pt-2.5">Google Reviews</h2>
+                <hr className="border border-secondary/5 my-[15px]" />
+                <Input
+                    label="URL"
+                    placeholder=""
+                    hideOptional={true}
+                    isRequired={true}
+                    icon="/images/add-link.svg"
+                />
+            </div>
+            <div className="flex gap-4 mt-[25px]">
+                <button className="text-lg leading-none w-full font-medium bg-danger border border-danger py-3 rounded-[10px] text-white">
+                    Disconnect
+                </button>
+                <button>
+                    <Image unoptimized={true} src="/images/edit.svg" alt="edit" width={46} height={46} />
+                </button>
+            </div>
+            {/* <button className="text-lg leading-none w-full font-medium bg-primary border border-primary hover:text-primary hover:bg-white py-3 rounded-[10px] mt-[25px] text-white">
+                            Connect
+                        </button> */}
         </div>
-    </AdminLayout>
-
     )
 }
