ec54735c642ba2cad069ca76152c12b00661d246
Bikramjit <bikramjit@fkointech.com>
2025-06-03 16:20:08 +0530

    validation to models

--- src/components/Models/review/StarBadgeWidget.js
+++ src/components/Models/review/StarBadgeWidget.js
@@ -6,84 +6,128 @@ import Checkbox from "../../form/Checkbox";
 import SecondaryButton from "../../common/SecondaryButton";
 import { useState } from "react";
 import CodePreviewBox from "./CodePreviewBox";
+import InputForm from "../../form/InputForm";
+import SelectForm from "../../form/SelectForm";
+import { useForm } from "react-hook-form";
+import CancelButton from "../../common/CancelButton";
+import { toast } from "react-toastify";
+import { getError } from "../../../../helper";
+import axios from "axios";
 
-export default function StarBadgeWidget({ onClose, OnSave }) {
+export default function StarBadgeWidget({ onClose, OnSave, id }) {
+    const { register, handleSubmit, clearErrors, formState: { errors } } = useForm();
+    const [sending, setSending] = useState(false)
     const [open, setOpen] = useState(false)
+
+    const onSubmit = async (data) => {
+        try {
+            setSending(true)
+            let res = null
+
+            if (id !== "add") {
+                res = await axios.put("/api", data)
+            } else {
+                res = await axios.post("/api", data)
+            }
+
+            toast.success("Saved Successfully")
+            setSending(false)
+            onClose()
+        } catch (error) {
+            toast.error(getError(error))
+            setSending(false)
+        }
+    }
 
     return (
         <Model onClose={onClose} title="Star Badge Widget" modalClass="w-[60%]!">
-            {open &&
-                <CodePreviewBox
-                    onClose={() => {
-                        setOpen(false)
-                    }}
+            <form onSubmit={handleSubmit(onSubmit)}>
+                {open &&
+                    <CodePreviewBox
+                        onClose={() => {
+                            setOpen(false)
+                        }}
 
-                    onSave={() => {
-                        setOpen(true)
-                    }} />
-            }
-            <div className="grid grid-cols-2 gap-4">
-                <div>
+                        onSave={() => {
+                            setOpen(true)
+                        }} />
+                }
+                <div className="grid grid-cols-2 gap-4">
                     <div>
-                        <div className="border border-border-color bg-[#0396FF1a] rounded-md">
-                            <div className="mt-2 pl-2 font-semibold text-lg">
-                                Design
+                        <div>
+                            <div className="border border-border-color bg-[#0396FF1a] rounded-md">
+                                <div className="mt-2 pl-2 font-semibold text-lg">
+                                    Design
+                                </div>
+
+                                <div className="grid grid-cols-3 gap-2 p-2">
+                                    <InputForm label="Color Scheme" isRequired={false} placeholder="Select Color"
+                                        formProps={{ ...register("colorScheme", { required: false }) }}
+                                        errors={errors} />
+                                    <SelectForm defaultOption="Select" label="Font Family" isRequired={false}
+                                        formProps={{ ...register("select", { required: false }) }}
+                                        errors={errors} clearErrors={clearErrors} >
+                                        <option value="selectFont">Select Font</option>
+                                    </SelectForm>
+                                    <InputForm label="Border Radius" isRequired={false} placeholder="Select Border Color"
+                                        formProps={{ ...register("borderRadius", { required: false }) }}
+                                        errors={errors} />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="border border-border-color bg-[#0396FF1a] rounded-md mt-2 ">
+                            <div className="font-semibold text-lg mt-2 pl-2">
+                                Review Sources
                             </div>
 
-                            <div className="grid grid-cols-3 gap-2 p-2">
-                                <Input label="Color Scheme" placeholder="Select Color" />
-                                <Select defaultOption="Select Font" label="Font Family" />
-                                <Input label="Border Radius" placeholder="Select Border Color" />
+                            <div className="pl-2">
+                                Select up to 3
                             </div>
+
+                            <div className="pl-2 flex gap-2 mb-2">
+                                <Checkbox />
+                                <div>Google</div>
+
+                                <Checkbox />
+                                <div>Trustpilot</div>
+
+                                <Checkbox />
+                                <div>Yelp</div>
+                            </div>
+                        </div>
+
+                        <div className="mt-2 grid grid-cols-2 gap-4">
+                            <SecondaryButton title="Save" type="submit" disabled={sending} />
+                            <CancelButton title="Next" />
                         </div>
                     </div>
 
-                    <div className="border border-border-color bg-[#0396FF1a] rounded-md mt-2 ">
-                        <div className="font-semibold text-lg mt-2 pl-2">
-                            Review Sources
+                    <div className="border border-border-color rounded-md">
+                        <div className='bg-[#0396FF1a] px-5 py-4 rounded-tl-[10px] rounded-tr-[10px]'>
+                            <div className='flex items-center gap-3'>
+                                <div className='text-secondary text-lg font-semibold'>Widget Preview</div>
+                            </div>
                         </div>
 
-                        <div className="pl-2">
-                            Select up to 3
+                        <div className='p-5'>
+                            <div className='border border-border-color rounded-[10px] p-5 text-secondary text-sm mb-8 leading-normal'>
+                                <div>Hi {"John Deo"},</div>
+
+                                <div className='my-5'>Thank you for your recent visit! We'd love to hear your feedback.</div>
+
+                                <div>Click the link below to leave a review:{"review_link"} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+
+                                <div className='mt-10'>business_name</div>
+                            </div>
                         </div>
 
-                        <div className="pl-2 flex gap-2 mb-2">
-                            <Checkbox />
-                            <div>Google</div>
-
-                            <Checkbox />
-                            <div>Trustpilot</div>
-
-                            <Checkbox />
-                            <div>Yelp</div>
+                        <div className="p-4">
+                            <SecondaryButton title="Get Code" onClick={() => { setOpen(true) }} />
                         </div>
                     </div>
                 </div>
-
-                <div className="border border-border-color rounded-md">
-                    <div className='bg-[#0396FF1a] px-5 py-4 rounded-tl-[10px] rounded-tr-[10px]'>
-                        <div className='flex items-center gap-3'>
-                            <div className='text-secondary text-lg font-semibold'>Widget Preview</div>
-                        </div>
-                    </div>
-
-                    <div className='p-5'>
-                        <div className='border border-border-color rounded-[10px] p-5 text-secondary text-sm mb-8 leading-normal'>
-                            <div>Hi {"John Deo"},</div>
-
-                            <div className='my-5'>Thank you for your recent visit! We'd love to hear your feedback.</div>
-
-                            <div>Click the link below to leave a review:{"review_link"} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
-
-                            <div className='mt-10'>business_name</div>
-                        </div>
-                    </div>
-
-                    <div className="p-4">
-                        <SecondaryButton title="Get Code" onClick={() => { setOpen(true) }} />
-                    </div>
-                </div>
-            </div>
+            </form>
         </Model >
     )
 }
