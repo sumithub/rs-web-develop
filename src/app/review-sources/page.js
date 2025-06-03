34c00453eba0f5ff729deec32f3a096bc74fb38e
Bikramjit <bikramjit@fkointech.com>
2025-06-03 11:58:52 +0530

    button functionality

--- src/app/review-sources/page.js
+++ src/app/review-sources/page.js
@@ -5,9 +5,13 @@ import CustomSelectBox from "../../components/form/CustomSelectBox";
 import Search from "../../components/form/Search";
 import Status from "../../components/Status";
 import Input from "../../components/form/Input";
+import { useState } from "react";
+import DisconnectReviewSourceConfirmation from "../../components/Models/review/DisconnectReviewSourceConfirmation";
+import ConnectReviewSource from "../../components/Models/review/ConnectReviewSource";
 
 export default function ReviewSources() {
     return (<AdminLayout>
+
         <div className="flex justify-between">
             <div className="flex gap-5 w-full">
                 <div className="flex gap-2.5 items-center">
@@ -47,7 +51,7 @@ export default function ReviewSources() {
             <div className="pt-5">
                 <h2 className="text-lg font-semibold">All Review Sources</h2>
                 <div className="pt-[15px] grid grid-cols-3 gap-y-[15px] gap-x-5">
-                    <ReviewCard />
+                    <ReviewCard status="disconnect" />
                     <ReviewCard />
                     <ReviewCard />
                     <ReviewCard />
@@ -57,12 +61,33 @@ export default function ReviewSources() {
     </AdminLayout >
     )
 }
-function ReviewCard() {
+function ReviewCard({ status }) {
+    const [openDisconnect, setOpenDisconnect] = useState(false)
+    const [openModal, setOpenModal] = useState(false)
+
     return (
         <div className="p-[15px] shadow-sm rounded-[15px]">
+            {openDisconnect &&
+                <DisconnectReviewSourceConfirmation
+                    onClose={() => {
+                        setOpenDisconnect(false)
+                    }}
+
+                    onSave={() => {
+                        setOpenDisconnect(true)
+                    }} />
+            }
+
+            {openModal &&
+                <ConnectReviewSource
+                    onClose={() => {
+                        setOpenModal(false)
+                    }}
+                />
+            }
             <div className="flex items-start justify-between">
                 <Image unoptimized={true} src="/images/google2.svg" alt="google2" width={87} height={36} />
-                <Status status="Connected" />
+                <Status status="connected" />
             </div>
             <div>
                 <h2 className="text-base font-medium pt-2.5">Google Reviews</h2>
@@ -76,17 +101,18 @@ function ReviewCard() {
                     infoIcon="/images/url.svg"
                 />
             </div>
-            <div className="flex gap-4 mt-[25px]">
-                <button className="text-lg leading-none w-full font-medium bg-danger border border-danger py-3 rounded-[10px] text-white">
+            {status === "disconnect" && < div className="flex gap-4 mt-[25px]">
+                <button className="text-lg leading-none w-full font-medium bg-danger border border-danger py-3 rounded-[10px] text-white"
+                    onClick={() => { setOpenDisconnect(true) }}>
                     Disconnect
                 </button>
                 <button>
-                    <Image unoptimized={true} src="/images/edit.svg" alt="edit" width={46} height={46} />
+                    <Image unoptimized={true} src="/images/edit.svg" alt="edit" width={46} height={46} onClick={() => { setOpenModal(true) }} />
                 </button>
-            </div>
-            {/* <button className="text-lg leading-none w-full font-medium bg-primary border border-primary hover:text-primary hover:bg-white py-3 rounded-[10px] mt-[25px] text-white">
-                            Connect
-                        </button> */}
-        </div>
+            </div>}
+            <button className="text-lg leading-none w-full font-medium bg-primary border border-primary hover:text-primary hover:bg-white py-3 rounded-[10px] mt-[25px] text-white" onClick={() => { setOpenModal(true) }}>
+                Connect
+            </button>
+        </div >
     )
 }
