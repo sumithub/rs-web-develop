47e43875711d511f966690a7e8b738e7b76c3ca6
rajni62 <rajni@fkointech.com>
2025-06-03 14:27:51 +0530

    EditCustomer view

--- src/components/Models/customers/AddCustomer.js
+++ src/components/Models/customers/AddCustomer.js
@@ -37,40 +37,44 @@ function AddCustomer({ onClose, id }) {
             setSending(false)
         }
     }
-    return <Model onClose={onClose} title="Add new Customer" modalClass="w-[65%]!">
+    return <Model onClose={onClose} title={`${!id ? "Add new Customer" : "Edit Customers List"}`} modalClass="w-[65%]!">
         <form onSubmit={handleSubmit(onSubmit)}>
             <div>
-                <div className="flex gap-4 items-center">
+                {!id && <div className="flex gap-4 items-center">
                     <button type="button"><Image src="/images/arrow-box.svg" alt="arrow" height={30} width={30} unoptimized={true} /></button>
 
                     <RadioForm
                         label="manually"
                         class_="mt-0!"
-
                     />
                     <RadioForm label="import"
                         class_="mt-0!"
 
                     />
-                </div>
+                </div>}
 
                 <div>
-                    <InputForm label="Customer Name" placeholder="Enter your name" isRequired={true}
-                        formProps={{ ...register("customerName", { required: true }) }}
-                        errors={errors} />
+                    {!id && <div>
+                        <InputForm label="Customer Name" placeholder="Enter your name" isRequired={true}
+                            formProps={{ ...register("customerName", { required: true }) }}
+                            errors={errors} />
 
-                    <InputForm label="Email" placeholder="Enter email" isRequired={true}
-                        formProps={{ ...register("email", { required: true }) }}
-                        errors={errors} />
+                        <InputForm label="Email" placeholder="Enter email" isRequired={true}
+                            formProps={{ ...register("email", { required: true }) }}
+                            errors={errors} />
 
-                    <PhoneForm label="Primary Phone"
-                        placeholder="Enter phone number"
-                        isRequired={true}
-                        formProps={{ ...register("phone", { required: true }) }}
-                        errors={errors}
-                        clearErrors={clearErrors}
-                        setValue={setValue}
-                        watch={watch} />
+                        <PhoneForm label="Primary Phone"
+                            placeholder="Enter phone number"
+                            isRequired={true}
+                            formProps={{ ...register("phone", { required: true }) }}
+                            errors={errors}
+                            clearErrors={clearErrors}
+                            setValue={setValue}
+                            watch={watch} />
+                    </div>}
+                    {id && <InputForm label="List Name" placeholder="Enter List Name" isRequired={true}
+                        formProps={{ ...register("listName", { required: true }) }}
+                        errors={errors} />}
 
                     <SelectForm label="Tag" isRequired={false} selectClass_="py-3.5! px-2.5! focus:border-primary/60!"
                         formProps={{ ...register("tag", { required: false }) }} errors={errors} clearErrors={clearErrors}>
@@ -79,6 +83,18 @@ function AddCustomer({ onClose, id }) {
                         <option value="instead of source">instead of source</option>
                     </SelectForm>
                 </div>
+
+                {id && <div>
+                    <div className="mt-4">
+                        <div className="text-sm text-secondary font-medium">Duplicate Handling<span className="text-danger">*</span></div>
+
+                        <div className="flex items-center gap-4">
+                            <RadioForm label="Ignore duplicates" class_="mt-2!" />
+                            <RadioForm label="Overwrite existing" class_="mt-2!" />
+                            <RadioForm label="Allow duplicates" class_="mt-2!" />
+                        </div>
+                    </div>
+                </div>}
 
                 {/* <div className="mt-8">
                     <ImportCustomer />
