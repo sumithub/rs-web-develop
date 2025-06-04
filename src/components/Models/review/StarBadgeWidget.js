91faa542b1bb17ef5201f7149bd22754a1d4aadd
Kulwinder Singh <kulwinder@fkointech.com>
2025-06-04 11:18:08 +0530

    tastimonial model ui

--- src/components/Models/review/StarBadgeWidget.js
+++ src/components/Models/review/StarBadgeWidget.js
@@ -13,6 +13,9 @@ import CancelButton from "../../common/CancelButton";
 import { toast } from "react-toastify";
 import { getError } from "../../../../helper";
 import axios from "axios";
+import Image from "next/image";
+import Switch from "../../form/Switch";
+import ProgressBar from "@ramonak/react-progress-bar";
 
 export default function StarBadgeWidget({ onClose, OnSave, id }) {
     const { register, handleSubmit, clearErrors, formState: { errors } } = useForm();
@@ -40,7 +43,7 @@ export default function StarBadgeWidget({ onClose, OnSave, id }) {
     }
 
     return (
-        <Model onClose={onClose} title="Star Badge Widget" modalClass="w-[60%]!">
+        <Model onClose={onClose} title="Star Badge Widget" modalClass="w-[80%]!">
             <form onSubmit={handleSubmit(onSubmit)}>
                 {open &&
                     <CodePreviewBox
@@ -52,78 +55,149 @@ export default function StarBadgeWidget({ onClose, OnSave, id }) {
                             setOpen(true)
                         }} />
                 }
-                <div className="grid grid-cols-2 gap-4">
+                <div className="grid grid-cols-2 gap-5">
                     <div>
-                        <div>
-                            <div className="border border-border-color bg-[#0396FF1a] rounded-md">
-                                <div className="mt-2 pl-2 font-semibold text-lg">
-                                    Design
-                                </div>
+                        <div className="p-[15px] bg-dark rounded-[15px]">
+                            <h2 className="text-lg font-semibold">Design</h2>
+                            <div className="grid grid-cols-3 gap-[15px] pt-2.5">
+                                <InputForm
+                                    label="Color Scheme"
+                                    // isRequired={true}
+                                    placeholder="Select Color"
+                                    formProps={{ ...register("colorScheme", { required: true }) }}
+                                    errors={errors}
+                                    inputClass="border border-primary3/10 p-2.5! bg-white!"
+                                    labelClass="pb-2.5! inline-block"
+                                    class_="mt-0!"
+                                />
+                                <SelectForm
+                                    defaultOption="Select"
+                                    label="Font Family"
+                                    labelClass="pb-2.5 inline-block mb-0!"
+                                    // isRequired={true}
+                                    formProps={{ ...register("select", { required: true }) }}
+                                    errors={errors}
+                                    class_="mt-0!"
+                                    selectClass_="border border-primary3/10 py-2.5! px-2.5! bg-white! text-sm!"
+                                    clearErrors={clearErrors} >
+                                    <option value="selectFont">Select Font</option>
+                                </SelectForm>
+                                <InputForm
+                                    label="Border Radius"
+                                    // isRequired={true}
+                                    placeholder="Select Border Color"
+                                    formProps={{ ...register("colorScheme", { required: true }) }}
+                                    errors={errors}
+                                    inputClass="border border-primary3/10 p-2.5! bg-white!"
+                                    labelClass="pb-2.5! inline-block"
+                                    class_="mt-0!"
+                                />
+                            </div>
+                        </div>
 
-                                <div className="grid grid-cols-3 gap-2 p-2">
-                                    <InputForm label="Color Scheme" isRequired={false} placeholder="Select Color"
-                                        formProps={{ ...register("colorScheme", { required: false }) }}
-                                        errors={errors} />
-                                    <SelectForm defaultOption="Select" label="Font Family" isRequired={false}
-                                        formProps={{ ...register("select", { required: false }) }}
-                                        errors={errors} clearErrors={clearErrors} >
-                                        <option value="selectFont">Select Font</option>
-                                    </SelectForm>
-                                    <InputForm label="Border Radius" isRequired={false} placeholder="Select Border Color"
-                                        formProps={{ ...register("borderRadius", { required: false }) }}
-                                        errors={errors} />
+                        <div className="p-[15px] bg-dark rounded-[15px] mt-5">
+                            <h2 className="text-lg font-semibold">Review Sources</h2>
+                            <h3 className="text-base pt-2.5 font-medium">Select up to 3</h3>
+                            <div className="flex gap-[15px] items-center pt-[15px]">
+                                <div className="flex gap-2.5 items-center">
+                                    <Checkbox />
+                                    <div>Google</div>
+                                </div>
+                                <div className="flex gap-2.5 items-center">
+                                    <Checkbox />
+                                    <div>Trustpilot</div>
+                                </div>
+                                <div className="flex gap-2.5 items-center">
+                                    <Checkbox />
+                                    <div>Yelp</div>
                                 </div>
                             </div>
                         </div>
 
-                        <div className="border border-border-color bg-[#0396FF1a] rounded-md mt-2 ">
-                            <div className="font-semibold text-lg mt-2 pl-2">
-                                Review Sources
-                            </div>
-
-                            <div className="pl-2">
-                                Select up to 3
-                            </div>
-
-                            <div className="pl-2 flex gap-2 mb-2">
-                                <Checkbox />
-                                <div>Google</div>
-
-                                <Checkbox />
-                                <div>Trustpilot</div>
-
-                                <Checkbox />
-                                <div>Yelp</div>
-                            </div>
-                        </div>
-
-                        <div className="mt-2 grid grid-cols-2 gap-4">
-                            <SecondaryButton title="Save" type="submit" disabled={sending} />
-                            <CancelButton title="Next" />
+                        <div className="mt-[30px] grid grid-cols-2 gap-5">
+                            <SecondaryButton title="Save" type="submit" disabled={sending} class_="bg-white! text-primary!" />
+                            <CancelButton title="Next" class_="bg-primary text-white border border-primary" />
                         </div>
                     </div>
 
-                    <div className="border border-border-color rounded-md">
-                        <div className='bg-[#0396FF1a] px-5 py-4 rounded-tl-[10px] rounded-tr-[10px]'>
-                            <div className='flex items-center gap-3'>
-                                <div className='text-secondary text-lg font-semibold'>Widget Preview</div>
-                            </div>
+                    <div className="shadow-sm rounded-[15px]">
+                        <div className="bg-primary/10 rounded-t-[15px] px-5 py-[18px] flex gap-2.5 items-center">
+                            <Image src="/images/eye1.svg" alt="eye1" width={22} height={22} />
+                            <h2 className="text-lg font-semibold">Widget Preview</h2>
                         </div>
+                        <div className="p-5">
+                            <div className="border border-border2 rounded-[10px] p-5">
+                                <div className="">
+                                    <div>
+                                        <div className="text-secondary text-xl font-medium mb-[15px]">Star Rating Badge</div>
+                                        <div className="flex">
+                                            <div className="text-secondary">
+                                                {/* <div className="text-xs font-semibold">Total</div> */}
+                                                <div className="text-[22px] font-semibold my-1">4.3</div>
+                                                <div className="flex items-center gap-1">
+                                                    <Image src="/images/star.svg" alt="star" height={14} width={14} unoptimized={true} />
+                                                    <Image src="/images/star.svg" alt="star" height={14} width={14} unoptimized={true} />
+                                                    <Image src="/images/star.svg" alt="star" height={14} width={14} unoptimized={true} />
+                                                    <Image src="/images/star.svg" alt="star" height={14} width={14} unoptimized={true} />  <Image src="/images/star.svg" alt="star" height={14} width={14} unoptimized={true} />
+                                                </div>
 
-                        <div className='p-5'>
-                            <div className='border border-border-color rounded-[10px] p-5 text-secondary text-sm mb-8 leading-normal'>
-                                <div>Hi {"John Deo"},</div>
+                                                <div className="flex items-center justify-between mt-2">
+                                                    <div>
+                                                        <div className="2xl:text-sm lg:text-[10px] mb-1.5">Ratings</div>
+                                                        <div className="2xl:text-base text-sm font-semibold">233</div>
+                                                    </div>
+                                                    <div>
+                                                        <div className="2xl:text-sm lg:text-[10px] capitalize mb-1.5">Rating With Reviews</div>
+                                                        <div className="2xl:text-base text-sm font-semibold">217</div>
+                                                    </div>
+                                                </div>
+                                            </div>
 
-                                <div className='my-5'>Thank you for your recent visit! We'd love to hear your feedback.</div>
+                                            <div>
+                                                <div className="grid grid-cols-[0.1fr_auto] items-center">
+                                                    <div className="text-text3 text-sm">5</div>
+                                                    <ProgressBar completed={100} bgColor="#FFC107" height="12px"
+                                                        isLabelVisible={false} borderRadius="2px"
+                                                    />
+                                                </div>
 
-                                <div>Click the link below to leave a review:{"review_link"} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                                                <div className="grid grid-cols-[0.1fr_auto] items-center my-1.5">
+                                                    <div className="text-text3 text-sm">4</div>
+                                                    <ProgressBar completed={80} bgColor="#FFC107" height="12px"
+                                                        isLabelVisible={false} borderRadius="2px"
+                                                    />
+                                                </div>
+                                                <div className="grid grid-cols-[0.1fr_auto] items-center">
+                                                    <div className="text-text3 text-sm">3</div>
+                                                    <ProgressBar completed={50} bgColor="#FFC107" height="12px"
+                                                        isLabelVisible={false} borderRadius="2px"
+                                                    />
+                                                </div>
 
-                                <div className='mt-10'>business_name</div>
+                                                <div className="grid grid-cols-[0.1fr_auto] items-center my-1.5">
+                                                    <div className="text-text3 text-sm">2</div>
+                                                    <ProgressBar completed={50} bgColor="#FFC107" height="12px"
+                                                        isLabelVisible={false} borderRadius="2px"
+                                                    />
+                                                </div>
+
+                                                <div className="grid grid-cols-{1fr_1.5fr} items-center">
+                                                    <div className="text-text3 text-sm">1</div>
+                                                    <div className="flex justify-between">
+                                                        <ProgressBar completed={50} bgColor="#FFC107" height="12px"
+                                                            isLabelVisible={false} borderRadius="2px"
+                                                        />
+                                                        <div className="text-text3 text-sm">0.5</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="mt-[30px]">
+                                    <SecondaryButton title="Get Code" onClick={() => { setOpen(true) }} type="button" />
+                                </div>
                             </div>
-                        </div>
-
-                        <div className="p-4">
-                            <SecondaryButton title="Get Code" onClick={() => { setOpen(true) }} />
                         </div>
                     </div>
                 </div>
