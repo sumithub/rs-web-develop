496360b618bbde3325e91a98c94062d23f999f6f
Kulwinder Singh <kulwinder@fkointech.com>
2025-06-06 11:33:03 +0530

    .

--- src/components/Models/reports/ResendReportEmail.js
+++ src/components/Models/reports/ResendReportEmail.js
@@ -104,7 +104,7 @@ export default function ResendReportEmail({ onClose, id, onSave }) {
                         formProps={{ ...register("addCustomMessage", { required: false }) }}
                         errors={errors}
                         rows={5}
-                        placeholder="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+                        placeholder="Add Custom Message"
                     />
                 </div>
 
