b3206792f063884cedc822d753a5a769e7bd9b66
Bikramjit <bikramjit@fkointech.com>
2025-06-10 15:22:31 +0530

    create alert page

--- src/app/alerts-management/page.js
+++ src/app/alerts-management/page.js
@@ -2,13 +2,41 @@
 import AdminLayout from "../../components/AdminLayout";
 import PaginationDemo from "../../components/Pagination";
 import TableOrder from "../../components/TableOrder";
-import Image from "next/image";
 import Search from "../../components/form/Search";
 import CustomSelectBox from "../../components/form/CustomSelectBox";
 import Status from "../../components/Status"
 import Checkbox from "../../components/form/Checkbox";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { getError } from "../../../helper";
+import { useEffect, useState } from "react";
+import Loading from "../../components/Loading";
+import { templates } from "../../constent/constArray";
 
 export default function AlertsManagement() {
+    const [type, setType] = useState("")
+    const [list, setList] = useState([])
+    const [search, setSearch] = useState("")
+    const [loading, setLoading] = useState(true);
+    const [sortBy, setSortBy] = useState("")
+
+    useEffect(() => {
+        getTemplate()
+    }, [search, type, sortBy])
+
+    const getTemplate = async () => {
+        try {
+            setLoading(true)
+            const res = await axios.get("/api")
+            setList(res.data || templates)
+            setLoading(false)
+
+        } catch (error) {
+            toast.error(getError(error))
+            setLoading(false)
+        }
+    }
+
     const Projects = [
         { id: "AL-001", name: "Acme Corp", location: "NYC", type: "New Review", message: "Low Rating Alert", status: "New", date: "Mar 03, 2024", action: "Dismiss" },
         { id: "AL-001", name: "Acme Corp", location: "NYC", type: "New Review", message: "Low Rating Alert", status: "New", date: "Mar 03, 2024", action: "Dismiss" },
@@ -32,36 +60,54 @@ export default function AlertsManagement() {
                         }}
                     />
                 </div>
-                <div className="flex gap-[15px]">
+                <div className="flex items-center gap-3">
                     <CustomSelectBox
-                        defaultOption="Filters"
-                        class_='mt-0! w-28!'
-                        selectClass_="py-2.5! px-2.5!"
-                        // value={filterBy}
+                        class_="mt-0!"
+                        defaultOption="Filter"
+                        value={type}
                         onChange={(e) => {
-                            setFilterBy(e.target.value)
-                        }}
-                    >
-                        <option value="tags">Tags</option>
-                        <option value="source: manual vs. imported">Source: Manual vs. Imported</option>
+                            setType(e.target.value)
+                        }}>
+                        <option value="read">Read</option>
+                        <option value="dismissed">Dismissed</option>
                     </CustomSelectBox>
-                    <button className="bg-primary border border-primary hover:bg-white hover:text-primary rounded-lg py-[10.5px] px-3 text-white text-xs text-center capitalize cursor-pointer disabled:pointer-events-none disabled:opacity-50"
-                    // onClick={() => { setOpen(true) }}
-                    >
-                        Mark as Read</button>
+
+                    <button className="bg-primary border border-primary hover:bg-white hover:text-primary rounded-lg py-[10.5px] px-3 text-white text-xs text-center capitalize cursor-pointer disabled:pointer-events-none disabled:opacity-50 shrink-0"
+                        onClick={() => { toast.success("Marked Successfully") }}>Mark As Read</button>
                 </div>
             </div>
             <div className='table-class mt-[15px]'>
-                <table className='w-full'>
+                {loading ? <Loading /> : (list?.length > 0 ? <table className='w-full'>
                     <thead>
                         <tr>
-                            <th><TableOrder title="ID" /></th>
-                            <th><TableOrder title="Client Name" /></th>
-                            <th><TableOrder title="Location" /></th>
-                            <th><TableOrder title="Event Type" /></th>
-                            <th><TableOrder title="Message" /></th>
-                            <th><TableOrder title="Status" /></th>
-                            <th><TableOrder title="Created Date" /></th>
+                            <th><TableOrder title="ID"
+                                setSortBy={setSortBy}
+                                sortBy={sortBy}
+                                field="id" /></th>
+                            <th><TableOrder title="Client Name"
+                                setSortBy={setSortBy}
+                                sortBy={sortBy}
+                                field="clientName" /></th>
+                            <th><TableOrder title="Location"
+                                setSortBy={setSortBy}
+                                sortBy={sortBy}
+                                field="location" /></th>
+                            <th><TableOrder title="Event Type"
+                                setSortBy={setSortBy}
+                                sortBy={sortBy}
+                                field="eventType" /></th>
+                            <th><TableOrder title="Message"
+                                setSortBy={setSortBy}
+                                sortBy={sortBy}
+                                field="message" /></th>
+                            <th><TableOrder title="Status"
+                                setSortBy={setSortBy}
+                                sortBy={sortBy}
+                                field="status" /></th>
+                            <th><TableOrder title="Created Date"
+                                setSortBy={setSortBy}
+                                sortBy={sortBy}
+                                field="createdDate" /></th>
                             <th>Actions</th>
                         </tr>
                     </thead>
@@ -87,9 +133,11 @@ export default function AlertsManagement() {
                                 </td>
                             </tr>)}
                     </tbody>
-                </table>
+                </table> : <div className='text-center text-2xl text-danger mx-auto py-20'>No Data</div>)}
             </div>
-            <PaginationDemo />
+            {list?.length > 0 && <div>
+                <PaginationDemo />
+            </div>}
         </AdminLayout>
     </>)
 }
