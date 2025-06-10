5b49c7c05c28a7ae703e6e24f10f2850587b1089
Bikramjit <bikramjit@fkointech.com>
2025-06-10 16:21:56 +0530

    create page and models

--- src/app/notification-preferences-management/page.js
+++ src/app/notification-preferences-management/page.js
@@ -2,12 +2,46 @@
 import AdminLayout from "../../components/AdminLayout";
 import PaginationDemo from "../../components/Pagination";
 import TableOrder from "../../components/TableOrder";
-import Image from "next/image";
 import Search from "../../components/form/Search";
 import CustomSelectBox from "../../components/form/CustomSelectBox";
 import Status from "../../components/Status"
+import Checkbox from "../../components/form/Checkbox";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { getError } from "../../../helper";
+import { useEffect, useState } from "react";
+import Loading from "../../components/Loading";
+import { templates } from "../../constent/constArray";
+import Image from "next/image";
+import EditNotificationPreferences from "../../components/Models/notification/EditNotificationPreference";
+import DeleteNotification from "../../components/Models/notification/DeleteNotification";
 
-export default function NotificationPreferencesManagement() {
+export default function NotificationManagement() {
+    const [type, setType] = useState("")
+    const [list, setList] = useState([])
+    const [search, setSearch] = useState("")
+    const [loading, setLoading] = useState(true);
+    const [sortBy, setSortBy] = useState("")
+    const [openEdit, setOpenEdit] = useState(false)
+    const [openDelete, setOpenDelete] = useState(false)
+
+    useEffect(() => {
+        getTemplate()
+    }, [search, type, sortBy])
+
+    const getTemplate = async () => {
+        try {
+            setLoading(true)
+            const res = await axios.get("/api")
+            setList(res.data || templates)
+            setLoading(false)
+
+        } catch (error) {
+            toast.error(getError(error))
+            setLoading(false)
+        }
+    }
+
     const Projects = [
         { id: "NP-001", name: "Acme Corp", location: "Global", type: "Email", enabled: "yes", date: "Mar 03, 2024", },
         { id: "NP-001", name: "Acme Corp", location: "Global", type: "Email", enabled: "yes", date: "Mar 03, 2024", },
@@ -21,6 +55,21 @@ export default function NotificationPreferencesManagement() {
     ]
     return (<>
         <AdminLayout>
+            {(openEdit === "edit") &&
+                <EditNotificationPreferences
+                    onClose={() => {
+                        setOpenEdit(false)
+                    }}
+                />
+            }
+
+            {(openDelete === "delete") &&
+                <DeleteNotification
+                    onClose={() => {
+                        setOpenDelete(false)
+                    }}
+                />
+            }
             <div className="flex justify-between items-center gap-11">
                 <div className="w-1/3">
                     <Search
@@ -31,35 +80,51 @@ export default function NotificationPreferencesManagement() {
                         }}
                     />
                 </div>
-                <div className="flex gap-[15px]">
+                <div className="flex items-center gap-3">
                     <CustomSelectBox
-                        defaultOption="Filters"
-                        class_='mt-0! w-28!'
-                        selectClass_="py-2.5! px-2.5!"
-                        // value={filterBy}
+                        class_="mt-0!"
+                        defaultOption="Filter"
+                        value={type}
                         onChange={(e) => {
-                            setFilterBy(e.target.value)
-                        }}
-                    >
-                        <option value="tags">Tags</option>
-                        <option value="source: manual vs. imported">Source: Manual vs. Imported</option>
+                            setType(e.target.value)
+                        }}>
+                        <option value="email">Email</option>
+                        <option value="sms">SMS</option>
+                        <option value="alert">Alert</option>
                     </CustomSelectBox>
-                    <button className="bg-primary border border-primary hover:bg-white hover:text-primary rounded-lg py-[10.5px] px-3 text-white text-xs text-center capitalize cursor-pointer disabled:pointer-events-none disabled:opacity-50"
-                    // onClick={() => { setOpen(true) }}
-                    >
-                        Edit Preferences</button>
+
+                    <button className="bg-primary border border-primary hover:bg-white hover:text-primary rounded-lg py-[10.5px] px-3 text-white text-xs text-center capitalize cursor-pointer disabled:pointer-events-none disabled:opacity-50 shrink-0"
+                        onClick={() => { setOpenEdit("edit") }} >Edit Preferences</button>
                 </div>
             </div>
             <div className='table-class mt-[15px]'>
-                <table className='w-full'>
+                {loading ? <Loading /> : (list?.length > 0 ? < table className='w-full' >
                     <thead>
                         <tr>
-                            <th><TableOrder title="ID" /></th>
-                            <th><TableOrder title="Client Name" /></th>
-                            <th><TableOrder title="Location" /></th>
-                            <th><TableOrder title="Preference Type" /></th>
-                            <th><TableOrder title="Enabled" /></th>
-                            <th><TableOrder title="Created Date" /></th>
+                            <th><TableOrder title="ID"
+                                setSortBy={setSortBy}
+                                sortBy={sortBy}
+                                field="id" /></th>
+                            <th><TableOrder title="Client Name"
+                                setSortBy={setSortBy}
+                                sortBy={sortBy}
+                                field="clientName" /></th>
+                            <th><TableOrder title="Location"
+                                setSortBy={setSortBy}
+                                sortBy={sortBy}
+                                field="location" /></th>
+                            <th><TableOrder title="Preference Type"
+                                setSortBy={setSortBy}
+                                sortBy={sortBy}
+                                field="preferenceType" /></th>
+                            <th><TableOrder title="Enabled"
+                                setSortBy={setSortBy}
+                                sortBy={sortBy}
+                                field="enabled" /></th>
+                            <th><TableOrder title="Created Date"
+                                setSortBy={setSortBy}
+                                sortBy={sortBy}
+                                field="createdDate" /></th>
                             <th>Actions</th>
                         </tr>
                     </thead>
@@ -76,20 +141,24 @@ export default function NotificationPreferencesManagement() {
                                 <td>{e.date}</td>
                                 <td>
                                     <div className='flex items-center gap-2'>
-                                        <button className='cursor-pointer'>
+                                        <button className='cursor-pointer'
+                                            onClick={() => { setOpenEdit("edit") }}>
                                             <Image src="/images/edit.svg" alt='edit' height={28} width={28} />
                                         </button>
 
-                                        <button className='cursor-pointer'>
+                                        <button className='cursor-pointer'
+                                            onClick={() => setOpenDelete("delete")}>
                                             <Image src="/images/delete1.svg" alt='delete' height={28} width={28} />
                                         </button>
                                     </div>
                                 </td>
                             </tr>)}
                     </tbody>
-                </table>
+                </table > : <div className='text-center text-2xl text-danger mx-auto py-20'>No Data</div>)}
             </div>
-            <PaginationDemo />
+            {list?.length > 0 && <div>
+                <PaginationDemo />
+            </div>}
         </AdminLayout>
     </>)
 }
