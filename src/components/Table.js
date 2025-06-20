daa60d32b4e542d44a2a24e2a74a776e4a3274df
Personal <personal@Meyrazs-MacBook-Pro.local>
2025-06-20 22:13:17 +1000

    checking git functionality

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,59 +1,79 @@
 import Image from "next/image";
 
 export default function Table() {
-    return <div>
-        <div className="mt-8 w-full border border-border-color rounded-[20px] overflow-hidden">
-            <table className="w-full">
-                <thead>
-                    <tr>
-                        <th>Source</th>
-                        <th>Total Reviews</th>
-                        <th>Last 30 Days</th>
-                        <th>This Month</th>
-                        <th>Last Month</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <div className="flex gap-2.5">
-                                <Image src="/images/google.svg" alt="google.svg" width={17} height={17} unoptimized={true} />
-                                Google
-                            </div>
-                        </td>
-                        <td>500</td>
-                        <td>300</td>
-                        <td>300</td>
-                        <td>123</td>
-                    </tr>
+  return (
+    <div>
+      <div className="mt-8 w-full border border-border-color rounded-[20px] overflow-hidden">
+        <table className="w-full">
+          <thead>
+            <tr>
+              <th>Source</th>
+              <th>Total Reviews</th>
+              <th>Last 30 Days</th>
+              <th>This Month</th>
+              <th>Last Month</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <div className="flex gap-2.5">
+                  <Image
+                    src="/images/google.svg"
+                    alt="google.svg"
+                    width={17}
+                    height={17}
+                    unoptimized={true}
+                  />
+                  Google -Xaxis
+                </div>
+              </td>
+              <td>500</td>
+              <td>300</td>
+              <td>300</td>
+              <td>123</td>
+            </tr>
 
-                    <tr>
-                        <td>
-                            <div className="flex gap-2.5">
-                                <Image src="/images/google.svg" alt="google.svg" width={17} height={17} unoptimized={true} />
-                                Google
-                            </div>
-                        </td>
-                        <td>500</td>
-                        <td>300</td>
-                        <td>300</td>
-                        <td>123</td>
-                    </tr>
+            <tr>
+              <td>
+                <div className="flex gap-2.5">
+                  <Image
+                    src="/images/google.svg"
+                    alt="google.svg"
+                    width={17}
+                    height={17}
+                    unoptimized={true}
+                  />
+                  Google
+                </div>
+              </td>
+              <td>500</td>
+              <td>300</td>
+              <td>300</td>
+              <td>123</td>
+            </tr>
 
-                    <tr>
-                        <td>
-                            <div className="flex gap-2.5">
-                                <Image src="/images/google.svg" alt="google.svg" width={17} height={17} unoptimized={true} />
-                                Google
-                            </div>
-                        </td>
-                        <td>500</td>
-                        <td>300</td>
-                        <td>300</td>
-                        <td>123</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+            <tr>
+              <td>
+                <div className="flex gap-2.5">
+                  <Image
+                    src="/images/google.svg"
+                    alt="google.svg"
+                    width={17}
+                    height={17}
+                    unoptimized={true}
+                  />
+                  Google
+                </div>
+              </td>
+              <td>500</td>
+              <td>300</td>
+              <td>300</td>
+              <td>123</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+  );
 }
