545dab15a8b088a40e6791e7cd9c98486ca51c4f
meyraz <meyraz2348@gmail.com>
2025-06-21 22:20:57 +1000

    MP1-151 Frontend: Form Field & Input Validation  MP1-205 Frontend: Test cases for Signup Page,

--- src/components/form/CheckboxForm.js
+++ src/components/form/CheckboxForm.js
@@ -1,28 +1,58 @@
-"use client"
-export default function CheckboxForm({ errors, id, label, name, checked, formProps, disabled, class_ = "", labelClass = "", inputClass = "", onChange }) {
+"use client";
+export default function CheckboxForm({
+  errors,
+  id,
+  label,
+  name,
+  checked,
+  formProps,
+  disabled,
+  class_ = "",
+  labelClass = "",
+  inputClass = "",
+  onChange
+}) {
+  let error = "";
 
-    let error = "";
-
-    if (formProps?.name && errors?.[formProps.name]) {
-        const fieldError = errors[formProps.name];
-        if (fieldError.type === "pattern" || fieldError.type === "validate" || fieldError.type === "required") {
-            error = fieldError.message || "This field is required";
-        } else {
-            error = "This field is required";
-        }
+  if (formProps?.name && errors?.[formProps.name]) {
+    const fieldError = errors[formProps.name];
+    if (
+      fieldError.type === "pattern" ||
+      fieldError.type === "validate" ||
+      fieldError.type === "required"
+    ) {
+      error = fieldError.message || "This field is required";
+    } else {
+      error = "This field is required";
     }
-    return <div>
-        <label className={`${class_} ${disabled ? "pointer-events-none" : ""}`} htmlFor={id || label}>
-            <div className="pointer-events-none">
-                <div className={`text-base text-secondary ${labelClass}`}>{label}</div>
-                <div className="text-base text-secondary font-bold">{name}</div>
-            </div>
-            <input id={id || label} disabled={disabled} type="checkbox" className={`h-4 w-5 ml-auto mt-1 ${inputClass}`}
-                {...formProps}
-                onChange={(e) => {
-                    if (onChange) onChange(e);
-                }}
-                checked={checked || false} />
-        </label>  {error && <p className="text-xs pt-[5px] capitalize text-danger">{error}</p>}
+  }
+  return (
+    <div>
+      <label
+        className={`${class_} ${disabled ? "pointer-events-none" : ""}`}
+        htmlFor={id || label}
+      >
+        <div className="pointer-events-none">
+          <div className={`text-base text-secondary ${labelClass}`}>
+            {label}
+          </div>
+          <div className="text-base text-secondary font-bold">{name}</div>
+        </div>
+        <input
+          id={id || label}
+          disabled={disabled}
+          type="checkbox"
+          className={`h-4 w-5 ml-auto mt-1 ${inputClass}`}
+          {...formProps}
+          onChange={(e) => {
+            if (onChange) onChange(e);
+          }}
+          checked={checked || false}
+        />
+      </label>{" "}
+      {error && (
+        <p className="text-xs pt-[5px] capitalize text-danger">{error}</p>
+      )}
     </div>
+  );
 }
