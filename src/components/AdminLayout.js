9d030f0481bf50f44dc73b1ae78fc0d47f9e35bc
Gurwinder Singh <whogurwinder@gmail.com>
2025-07-01 12:01:04 +0530

    role setup

--- src/components/AdminLayout.js
+++ src/components/AdminLayout.js
@@ -1,18 +1,33 @@
 "use client"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Loading from "./Loading";
+import { useRouter } from 'next/navigation';
 
 export default function AdminLayout({ children, headerChild, headerSearch, cardClass = "", noCard = false, class_ = "", mainClass = "" }) {
     const [collapse, setCollapse] = useState(false)
+    const [role, setRole] = useState("loading")
+    const router = useRouter();
 
     const toggleSidebar = () => {
         setCollapse(!collapse)
     }
+    useEffect(() => {
+        const r = localStorage.getItem("role")
+        if (!r) {
+            router.push(`/login`)
+        } else {
+            setRole(r)
+        }
+    }, [])
 
+    if (role === "loading") {
+        return <Loading />
+    }
     return <div className="bg-light">
         <Header headerChild={headerChild} headerSearch={headerSearch} collapse={collapse} />
-        <Sidebar collapse={collapse} toggleSidebar={toggleSidebar} />
+        <Sidebar collapse={collapse} toggleSidebar={toggleSidebar} role={role} />
         <div className={`pb-2 min-h-screen px-0`}>
             <div className={`py-4  ${mainClass}`}>
                 <div className={`${collapse ? "pl-[92px]" : "pl-[305px]"} pr-3 mt-[85px] ${class_}`}>
