977a9e3e4b07b00e015aea70a890343fed9cec90
Kulwinder Singh <kulwinder@fkointech.com>
2025-07-09 16:33:26 +0530

    review page fixed

--- src/app/review-sources/page.js
+++ src/app/review-sources/page.js
@@ -36,13 +36,12 @@ export default function ReviewSources() {
     return (<AdminLayout>
 
         <div className="flex justify-between">
-            <div className="flex gap-5 w-full">
-                <div className="flex gap-2.5 items-center">
-                    <h2 className="text-lg capitalize font-medium shrink-0">Manage Review Sources</h2>
-                    <div className="text-primary/10">|</div>
-                    <h2 className="text-lg capitalize font-medium shrink-0">Connect your business to review platforms</h2>
-                </div>
-                <div className="w-1/4">
+            <div className="">
+                <h2 className="text-lg capitalize font-medium shrink-0">Manage Review Sources</h2>
+                <h2 className="text-sm text-text3 pt-1 capitalize">Connect your business to review platforms</h2>
+            </div>
+            <div className=" flex gap-3.5">
+                <div className="w-80">
                     <Search
                         mainClass='w-full!'
                         placeholder="Search by Review Sources"
@@ -51,8 +50,6 @@ export default function ReviewSources() {
                         }}
                     />
                 </div>
-            </div>
-            <div>
                 <CustomSelectBox
                     defaultOption="Filters"
                     class_='mt-0! w-26!'
