4dd4af52765c1dc3ca613b48c9dfd8b58a29ab05
Kulwinder Singh <kulwinder@fkointech.com>
2025-07-10 14:56:49 +0530

    gap fixed

--- src/app/admin/business-management/customer-tagging/page.js
+++ src/app/admin/business-management/customer-tagging/page.js
@@ -274,7 +274,7 @@ function CustomerTagging() {
                                         colors={["#0396FF", "#16C098", "#FFAE4C", "#07DBFA", "#988AFC"]}
                                     />
                                 </div>
-                                <div className="mt-10 w-[55%]">
+                                <div className="mt-10 w-[55%] flex flex-col gap-3">
                                     <div className="flex items-center justify-between gap-3 mb-2">
                                         <div className='flex gap-2.5 items-center'>
                                             <div className="bg-primary h-3 w-3 rounded-full"></div>
