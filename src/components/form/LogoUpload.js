1c8a314367359b77ca6b920e148aaf4c92cccdb3
Kulwinder Singh <kulwinder@fkointech.com>
2025-07-12 15:44:00 +0530

    fixed

--- src/components/form/LogoUpload.js
+++ src/components/form/LogoUpload.js
@@ -214,13 +214,14 @@ export default function LogoUpload({
                     onClick={handleChooseFile}
                 >
                     <div className="flex flex-col items-center">
-                        <div className="w-12 h-12 bg-blue-100 rounded-full flex items-center justify-center mb-3.5">
+                        {/* <div className="w-12 h-12 bg-blue-100 rounded-full flex items-center justify-center mb-3.5">
                             <svg className="w-6 h-6 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                             </svg>
-                        </div>
+                        </div> */}
+                        <Image src="/images/upload.svg" alt="upload" width={32} height={32} />
 
-                        <div className="text-lg font-semibold mb-2.5">
+                        <div className="text-lg font-semibold mb-2.5 mt-3.5">
                             Drag & Drop Or Choose Logo To Upload
                         </div>
 
@@ -262,33 +263,34 @@ export default function LogoUpload({
                                     // </svg>
                                 )}
                             </div>
-                            <span className="text-lg font-semibold text-gray-800">ABC Solutions</span>
+                            <span className="text-lg font-semibold ">ABC Solutions</span>
                         </div>
                     </div>
 
                     {/* File details section */}
-                    <div className="flex items-center justify-between p-3.5 bg-white rounded-lg border border-border2">
+                    <div className="flex items-center justify-between p-3.5 bg-white rounded-lg border border-primary/10">
                         <div className="flex items-center gap-3">
                             {/* File icon */}
-                            <div className="w-10 h-10 bg-green-100 rounded flex items-center justify-center">
+                            {/* <div className="w-10 h-10 bg-green-100 rounded flex items-center justify-center">
                                 <svg className="w-5 h-5 text-green-600" fill="currentColor" viewBox="0 0 20 20">
                                     <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clipRule="evenodd" />
                                 </svg>
-                            </div>
+                            </div> */}
+                            <Image src="/images/csv.svg" alt='csv' width={30} height={35} />
 
                             {/* File info */}
                             <div>
-                                <p className="text-sm font-medium text-gray-800">
+                                <p className="text-sm font-medium capitalize ">
                                     {selectedFile.name}
                                 </p>
-                                <p className="text-xs text-gray-500">
+                                <p className="text-xs font-medium text-text3">
                                     {(selectedFile.size / 1024).toFixed(2)} KB
                                 </p>
                             </div>
                         </div>
 
                         {/* Success checkmark */}
-                        <div className="w-12 h-12 bg-green-100 rounded-full flex items-center justify-center">
+                        {/* <div className="w-12 h-12 bg-green-100 rounded-full flex items-center justify-center">
                             <svg className="w-6 h-6 text-green-600" fill="currentColor" viewBox="0 0 20 20">
                                 <path
                                     fillRule="evenodd"
@@ -296,14 +298,16 @@ export default function LogoUpload({
                                     clipRule="evenodd"
                                 />
                             </svg>
-                        </div>
+                        </div> */}
+                        <Image src="/images/checked.svg" alt='checked' width={30} height={30} />
+
                     </div>
 
                     {/* Action buttons */}
                     <div className="flex items-center justify-between mt-6">
                         {/* <button
                             onClick={handleRemoveFile}
-                            className="text-gray-500 text-sm hover:text-gray-700 transition-colors"
+                            className=" text-sm hover:text-gray-700 transition-colors"
                             type="button"
                         >
                             Cancel
